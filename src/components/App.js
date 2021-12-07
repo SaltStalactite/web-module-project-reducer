3baae57273bf96ec99d8e9d6e4543fa43aef7b72
cc <votaryofturms@icloud.com>
2021-12-07 02:15:28 -0500

    changed 0 in memory to reference to state.memory

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,7 @@ function App() {
             <TotalDisplay value={0} />
             <div className="row details">
               <span id="operation"><b>Operation:</b>{state.operation}</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="memory"><b>Memory:</b>{state.memory}</span>
             </div>
 
             <div className="row">
